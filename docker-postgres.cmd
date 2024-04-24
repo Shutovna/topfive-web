cdbd3a2c1a76141f2bb6a986014d54abfc8826f7
Nikita Shutov <ShutovNA1987@gmail.com>
2024-04-24 10:08:16 +0700

    Change db name and user

--- docker-postgres.cmd
+++ docker-postgres.cmd
@@ -1,2 +1,2 @@
-docker run -dp 5432:5432 --name topfive-db -e POSTGRES_PASSWORD=password postgres
+docker run -dp 5432:5432 --name topfive-db -e POSTGRES_USER=topfive -e POSTGRES_PASSWORD=password -e POSTGRES_DB=topfive postgres
 docker start topfive-db
